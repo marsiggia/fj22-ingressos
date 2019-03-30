d40b14c52c23138574275b489091d726aaab5cd0
Marcio Siggia <marsiggia001@gmail.com>
2019-03-30 10:13:52 -0300

    Criado SessaoController e Sessao modelo, para conseguir mostrar a tela para cadastro de uma sessão

---
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -0,0 +1,31 @@
+package br.com.caelum.ingresso.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+
+@Controller
+public class SessaoController {
+	
+	@Autowired
+	private FilmeDao filmeDao;
+	
+	@Autowired
+	private SalaDao salaDao;
+	
+	@GetMapping("admin/sessao/")
+	public ModelAndView getSessoes(@RequestParam("salaId") Integer salaId) {
+		ModelAndView mdv = new ModelAndView("sessao/sessao");
+		
+		mdv.addObject("filmes", filmeDao.findAll());
+		mdv.addObject("sala", salaDao.findOne(salaId));
+		
+		return mdv;
+	}
+
+}
